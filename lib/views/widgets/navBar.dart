abfe011c04becb19e81ef997a808707b2b05708e
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-03-08 01:48:49 -0400

    added logo to navbar

--- lib/views/widgets/navBar.dart
+++ lib/views/widgets/navBar.dart
@@ -6,7 +6,17 @@ class NavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      height: 100,
+      color: Color.fromRGBO(4, 31, 81, 1),
+      child: Row(
+        children: [
+          SizedBox(
+            height: 80,
+            width: 150,
+            child: Image.asset('assets/app_logo.png'),
+          )
+        ]
+      ),
     );
   }
 }
