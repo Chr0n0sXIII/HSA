0e271d72fb5d257476bd7c9b6956a1ceb3da62b9
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-03-11 08:37:17 -0400

    updated packags

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:home_service_app/views/homeView.dart';
 import 'package:home_service_app/views/loginView.dart';
 
 void main() {
@@ -13,8 +14,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: loginView(),
+    return const MaterialApp(
+      home: HomeView(),
     );
   }
 }
