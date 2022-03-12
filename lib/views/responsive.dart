047762b968d16ef63bbebe6c69bc95713b79c2a6
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-03-11 20:40:08 -0400

    uploaded Repsonsive file

---
+++ lib/views/responsive.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Responsive extends StatelessWidget {
+  final Widget mobile;
+  final Widget desktop;
+
+  const Responsive({
+    Key? key,
+    required this.mobile,
+    required this.desktop,
+  }) : super(key: key);
+
+// This size work fine on my design, maybe you need some customization depends on your design
+
+  static bool isDesktop(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 1200;
+
+  @override
+  Widget build(BuildContext context) {
+    final Size _size = MediaQuery.of(context).size;
+    if (_size.width >= 1200) {
+      return desktop;
+    }
+    else {
+      return mobile;
+    }
+  }
+}
