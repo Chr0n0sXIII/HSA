31612ce2258c53c2ced09758040e31bfe9016d3c
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-03-20 22:54:09 -0400

    Accept Worker Tile

--- lib/views/widgets/homeView_TileClusterWidget.dart
+++ lib/views/widgets/homeView_TileClusterWidget.dart
@@ -90,7 +90,43 @@ class AcceptWorksTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      height: 150,
+      width: 450,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(7.0,8.0)
+          )
+        ]
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Icon(
+              Icons.person_add,
+              color: Color.fromRGBO(195, 166, 96,1),
+              size: 100,
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Text(
+              'Accept Workers',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 32,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
