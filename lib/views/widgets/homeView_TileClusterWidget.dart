8ea66fb05747d0e93db7e1532a46a08babf3020f
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-03-20 23:54:47 -0400

    User Peofile tile and padding

--- lib/views/widgets/homeView_TileClusterWidget.dart
+++ lib/views/widgets/homeView_TileClusterWidget.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class TileCluster extends StatefulWidget {
-  const TileCluster({ Key? key }) : super(key: key);
+  const TileCluster({Key? key}) : super(key: key);
 
   @override
   State<TileCluster> createState() => _TileClusterState();
@@ -10,220 +11,289 @@ class TileCluster extends StatefulWidget {
 class _TileClusterState extends State<TileCluster> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        UserTile(),
-        EditJobTile(),
-        AcceptWorksTile(),
-        Row(
-          children: [
-            CompleteJobTile(),
-            AddNewJobTile()
-          ],
-        )
-      ],
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        children: [
+          UserTile(),
+          EditJobTile(),
+          AcceptWorksTile(),
+          Row(
+            children: [CompleteJobTile(), AddNewJobTile()],
+          )
+        ],
+      ),
     );
   }
 }
 
 class UserTile extends StatefulWidget {
-  const UserTile({ Key? key }) : super(key: key);
+  const UserTile({Key? key}) : super(key: key);
 
   @override
   State<UserTile> createState() => _UserTileState();
 }
 
 class _UserTileState extends State<UserTile> {
+  String user_Skils = 'User Trade Skills and Profession';
+  String user_desc = 'User Description about me';
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Padding(
+      padding: EdgeInsets.all(5),
+      child: Container(
+        height: 140,
+        width: 450,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(7.0, 8.0))
+            ]),
+        child: Row(
+          children: [
+            SizedBox(
+              height: 100,
+              width: 100,
+              child: Image.asset('assets/profile_picture_place_holder.png',
+              scale: 0.65
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                RatingBar.builder(
+                  initialRating: 3,
+                  minRating: 1,
+                  direction: Axis.horizontal,
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                  itemBuilder: (context, _) => Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  ),
+                  onRatingUpdate: (rating) {
+                    print(rating);
+                  },
+                ),
+                Text(
+                  user_Skils,
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                Text(
+                  user_desc,
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 14
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
 class EditJobTile extends StatelessWidget {
-  const EditJobTile({ Key? key }) : super(key: key);
+  const EditJobTile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 150,
-      width: 450,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.all(Radius.circular(30)),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 5,
-            blurRadius: 7,
-            offset: Offset(7.0,8.0)
-          )
-        ]
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            Icons.border_color_outlined,
-            color: Color.fromRGBO(195, 166, 96,1),
-            size: 100,
-          ),
-          Text(
-            'View Your Job Listings \nand Edit',
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              fontSize: 32,
+    return Padding(
+      padding: EdgeInsets.all(5),
+      child: Container(
+        height: 140,
+        width: 450,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(7.0, 8.0))
+            ]),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.border_color_outlined,
+              color: Color.fromRGBO(195, 166, 96, 1),
+              size: 100,
             ),
-          )
-        ],
+            Text(
+              'View Your Job Listings \nand Edit',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 32,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 }
 
 class AcceptWorksTile extends StatelessWidget {
-  const AcceptWorksTile({ Key? key }) : super(key: key);
+  const AcceptWorksTile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 150,
-      width: 450,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.all(Radius.circular(30)),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 5,
-            blurRadius: 7,
-            offset: Offset(7.0,8.0)
-          )
-        ]
-      ),
-      child: Row(
-        children: [
-          Expanded(
-            flex: 1,
-            child: Icon(
-              Icons.person_add,
-              color: Color.fromRGBO(195, 166, 96,1),
-              size: 100,
-            ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Text(
-              'Accept Workers',
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 32,
+    return Padding(
+      padding: EdgeInsets.all(5),
+      child: Container(
+        height: 140,
+        width: 450,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(7.0, 8.0))
+            ]),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Icon(
+                Icons.person_add,
+                color: Color.fromRGBO(195, 166, 96, 1),
+                size: 100,
               ),
             ),
-          )
-        ],
+            Expanded(
+              flex: 3,
+              child: Text(
+                'Accept Workers',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 32,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 }
 
 class CompleteJobTile extends StatelessWidget {
-  const CompleteJobTile({ Key? key }) : super(key: key);
+  const CompleteJobTile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Padding(
+      padding: EdgeInsets.all(5),
+      child: Container(
         height: 150,
-      width: 215,
-      decoration: BoxDecoration(
-        color: Color.fromRGBO(5, 190, 231, 1),
-        borderRadius: BorderRadius.all(Radius.circular(30)),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 5,
-            blurRadius: 7,
-            offset: Offset(7.0,8.0)
-          )
-        ]
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Completed',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 34,
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Icon(
-              Icons.check_box,
-              color: Colors.white,
-              size: 50,
-              ),
-              Text(
-                'Jobs',
-                style: TextStyle(
+        width: 215,
+        decoration: BoxDecoration(
+            color: Color.fromRGBO(5, 190, 231, 1),
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(7.0, 8.0))
+            ]),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Completed',
+              style: TextStyle(
                 color: Colors.white,
                 fontWeight: FontWeight.bold,
                 fontSize: 34,
+              ),
             ),
-          ),
-            ],
-          )
-        ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(
+                  Icons.check_box,
+                  color: Colors.white,
+                  size: 50,
+                ),
+                Text(
+                  'Jobs',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 34,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
 }
 
 class AddNewJobTile extends StatelessWidget {
-  const AddNewJobTile({ Key? key }) : super(key: key);
+  const AddNewJobTile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 150,
-      width: 215,
-      decoration: BoxDecoration(
-        color: Color.fromRGBO(7, 223, 81, 1),
-        borderRadius: BorderRadius.all(Radius.circular(30)),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 5,
-            blurRadius: 7,
-            offset: Offset(7.0,8.0)
-          )
-        ]
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            Icons.add_box_rounded,
-            color: Colors.white,
-            size: 50,
-          ),
-          Text(
-            'Add  a  New\n Job Listing',
-            textAlign: TextAlign.center,
-            style: TextStyle(
+    return Padding(
+      padding: EdgeInsets.all(5),
+      child: Container(
+        height: 150,
+        width: 215,
+        decoration: BoxDecoration(
+            color: Color.fromRGBO(7, 223, 81, 1),
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(7.0, 8.0))
+            ]),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.add_box_rounded,
               color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 34,
+              size: 50,
             ),
-          ),
-        ],
+            Text(
+              'Add  a  New\n Job Listing',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 34,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
