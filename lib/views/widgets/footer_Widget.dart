e24010b73d8661277c0f377706ff042029c0851b
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-03-23 21:08:41 -0400

    added padding to the top of the footer for spacing

--- lib/views/widgets/footer_Widget.dart
+++ lib/views/widgets/footer_Widget.dart
@@ -5,78 +5,81 @@ class Footer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      height: 100,
-      decoration: BoxDecoration(
-        color: Color.fromRGBO(51, 51, 51, 1),
-        borderRadius: BorderRadius.only(topRight: Radius.circular(20), topLeft: Radius.circular(20))),
-      child: Row(
-        children: [
-          Expanded(
-            flex: 1,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Padding(
-                  padding: EdgeInsets.fromLTRB(5, 0, 0, 5),
-                  child: Text(
-                    'Terms Of Service',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white ,
-                      decoration: TextDecoration.underline
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 100, 0, 0),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        height: 100,
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(51, 51, 51, 1),
+          borderRadius: BorderRadius.only(topRight: Radius.circular(20), topLeft: Radius.circular(20))),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(5, 0, 0, 5),
+                    child: Text(
+                      'Terms Of Service',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white ,
+                        decoration: TextDecoration.underline
+                      ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(5, 0, 0, 5),
-                  child: Text(
-                    'About Us',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      decoration: TextDecoration.underline
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(5, 0, 0, 5),
+                    child: Text(
+                      'About Us',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        decoration: TextDecoration.underline
+                      ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(5, 0, 0, 5),
-                  child: Text(
-                    'Contact Us',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      decoration: TextDecoration.underline
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(5, 0, 0, 5),
+                    child: Text(
+                      'Contact Us',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        decoration: TextDecoration.underline
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            flex: 4,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Text(
-                  'Demo Information',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30, 
-                    fontWeight: FontWeight.bold,
-                  ),
-                )
-              ],
+            Expanded(
+              flex: 4,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    'Demo Information',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30, 
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          const Expanded(
-            flex: 1,
-            child: SizedBox(),
-          )
-        ],
+            const Expanded(
+              flex: 1,
+              child: SizedBox(),
+            )
+          ],
+        ),
       ),
     );
   }
