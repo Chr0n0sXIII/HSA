61217fd637e274fb3d55200452488a18a0213bb4
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-03-25 19:01:28 -0400

    Job Description and Type of service

--- lib/views/widgets/addJob_FormWidget.dart
+++ lib/views/widgets/addJob_FormWidget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Add_Job_Form extends StatefulWidget {
-  const Add_Job_Form({ Key? key }) : super(key: key);
+  const Add_Job_Form({Key? key}) : super(key: key);
 
   @override
   State<Add_Job_Form> createState() => _Add_Job_FormState();
@@ -9,6 +9,16 @@ class Add_Job_Form extends StatefulWidget {
 
 class _Add_Job_FormState extends State<Add_Job_Form> {
   String? Job_Title_Error;
+  List<String> items = [
+    'Yark Work',
+    'Cleaning',
+    'Landscaping',
+    'Painting',
+    'Maintainace',
+    'Plumbing',
+    'Misc'
+  ];
+  String? selectedItem = "Plumbing";
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -23,16 +33,38 @@ class _Add_Job_FormState extends State<Add_Job_Form> {
                   maxLength: 40,
                   maxLines: 1,
                   decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    filled:  true,
-                    label: Text("Job Title"),
-                    errorText: Job_Title_Error,
-                    border: OutlineInputBorder(
-                      borderRadius: 
-                        BorderRadius.all(Radius.circular(5))
-                    )
-                  ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      label: Text("Job Title"),
+                      errorText: Job_Title_Error,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5)))),
                 ),
+              ),
+              Container(
+                width: 400,
+                child: TextField(
+                  maxLength: 250,
+                  maxLines: 4,
+                  decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      label: Text("Job Description"),
+                      errorText: Job_Title_Error,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5)))),
+                ),
+              ),
+              DropdownButton<String>(
+                value: selectedItem,
+                items: items.map((String item) => DropdownMenuItem<String>(
+                  value: item,
+                  child: Text(item))).toList(),
+                onChanged: (item) => 
+                setState(() {
+                  selectedItem = item;
+                  }
+                )
               )
             ],
           )
