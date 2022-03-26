485cfc2be9ae7914a6d5c850b9404feb1f489692
kobe <kobegay66@live.com>
2022-03-25 20:43:49 -0400

    W.I.P.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:home_service_app/views/loginView.dart';
 
 void main() {
-  runApp(DevicePreview(
-    builder: (context) => const MyApp(),
-  ));
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
