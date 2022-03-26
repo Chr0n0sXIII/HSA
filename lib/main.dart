9065f6485da4080b801bd638fb8126d4450ba17e
kobe <kobegay66@live.com>
2022-03-25 20:40:40 -0400

    W.I.P

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:home_service_app/views/views.dart';
+import 'package:home_service_app/views/widgets/acceptWorkers_ScrollWidget.dart';
 
 void main() {
-  runApp(DevicePreview(
-    builder: (context) => const MyApp(),
-  ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,8 +11,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: AcceptWorkersView(),
-    );
+    return MaterialApp(home: AcceptWorkerView());
   }
 }
