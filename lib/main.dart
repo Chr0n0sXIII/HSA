a35099dbfa8a05054106cb7e7ac26403fca441d9
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-03-25 20:31:32 -0400

    working on image picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:home_service_app/views/homeView.dart';
-import 'package:home_service_app/views/loginView.dart';
+import 'package:home_service_app/views/views.dart';
 
 void main() {
-  runApp(DevicePreview(
-    builder: (context) => const MyApp(),
-  ));
+  runApp( MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomeView(),
+      home: Add_New_Job_View(),
     );
   }
 }
--- lib/views/widgets/addJob_FormWidget.dart
+++ lib/views/widgets/addJob_FormWidget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker_web/image_picker_web.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class Add_Job_Form extends StatefulWidget {
   const Add_Job_Form({Key? key}) : super(key: key);
@@ -19,12 +21,41 @@ class _Add_Job_FormState extends State<Add_Job_Form> {
     'Misc'
   ];
   String? selectedItem = "Plumbing";
+  List<Image>? fromPicker;
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Row(
         children: [
-          Column(),
+          Column(
+            children: [
+              Container(
+                width: 400,
+                color: Colors.grey[400],
+                child: CarouselSlider(
+                    items: fromPicker, options: CarouselOptions(height: 500)),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.blue[900]
+                    ),
+                    onPressed: () async {
+                      fromPicker =
+                          await ImagePickerWeb.getMultiImagesAsWidget();
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        'Upload Images',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    )),
+              )
+            ],
+          ),
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -41,7 +72,8 @@ class _Add_Job_FormState extends State<Add_Job_Form> {
                         label: Text("Job Title..."),
                         errorText: Job_Title_Error,
                         border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(5)))),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(5)))),
                   ),
                 ),
               ),
@@ -58,23 +90,22 @@ class _Add_Job_FormState extends State<Add_Job_Form> {
                         label: Text("Job Description..."),
                         errorText: Job_Title_Error,
                         border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(5)))),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(5)))),
                   ),
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: DropdownButton<String>(
-                  value: selectedItem,
-                  items: items.map((String item) => DropdownMenuItem<String>(
-                    value: item,
-                    child: Text(item))).toList(),
-                  onChanged: (item) => 
-                  setState(() {
-                    selectedItem = item;
-                    }
-                  )
-                ),
+                    value: selectedItem,
+                    items: items
+                        .map((String item) => DropdownMenuItem<String>(
+                            value: item, child: Text(item)))
+                        .toList(),
+                    onChanged: (item) => setState(() {
+                          selectedItem = item;
+                        })),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -89,37 +120,38 @@ class _Add_Job_FormState extends State<Add_Job_Form> {
                         label: Text("Price..."),
                         errorText: Job_Title_Error,
                         border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(5)))),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(5)))),
                   ),
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: ElevatedButton(
-                  onPressed: (){}, 
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Request Location',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  )
-                ),
+                    onPressed: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Request Location',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    )),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                  primary: Color.fromRGBO(11, 206, 131, 1)),
-                  onPressed: (){}, 
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      'Submit',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  )
-                ),
+                    style: ElevatedButton.styleFrom(
+                        primary: Color.fromRGBO(11, 206, 131, 1)),
+                    onPressed: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        'Submit',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    )),
               )
             ],
           )
