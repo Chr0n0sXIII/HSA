76529b9328cd350f8226bc62c605b2c227f371b7
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-04-08 20:12:50 -0400

    initial work form setup

---
+++ lib/views/widgets/workerReview_FormWidget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class Worker_Review_Fo extends StatefulWidget {
+  const Worker_Review_Fo({ Key? key }) : super(key: key);
+
+  @override
+  State<Worker_Review_Fo> createState() => _Worker_Review_FoState();
+}
+
+class _Worker_Review_FoState extends State<Worker_Review_Fo> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(),
+        Column(),
+      ],
+    );
+  }
+}
