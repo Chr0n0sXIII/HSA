6fd0992c5fcc5d6ff8cd590c01d8e25ec6495e28
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-04-10 03:53:44 -0400

    initial page

---
+++ lib/views/completedJobsView.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:home_service_app/views/widgets/navBar.dart';
+import 'package:home_service_app/views/widgets/footer_Widget.dart';
+import 'package:home_service_app/views/widgets/pageTitle_Widget.dart';
+
+class Complete_Job_List_View extends StatefulWidget {
+  const Complete_Job_List_View({ Key? key }) : super(key: key);
+
+  @override
+  State<Complete_Job_List_View> createState() => _Complete_Job_List_ViewState();
+}
+
+class _Complete_Job_List_ViewState extends State<Complete_Job_List_View> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(229, 229, 229, 1),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TopBar(),
+            PageTitle("View Your Completed Jobs"),
+            Footer(),
+          ],
+        )
+      ),
+    );
+  }
+}
