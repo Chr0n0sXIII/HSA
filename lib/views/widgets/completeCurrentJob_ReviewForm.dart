8a79c4ac54873fb18146b6a747e6b835841e8833
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-04-09 20:45:17 -0400

    initial form

---
+++ lib/views/widgets/completeCurrentJob_ReviewForm.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+class Complete_Job_Form extends StatefulWidget {
+  const Complete_Job_Form({ Key? key }) : super(key: key);
+
+  @override
+  State<Complete_Job_Form> createState() => _Complete_Job_FormState();
+}
+
+class _Complete_Job_FormState extends State<Complete_Job_Form> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(),
+        Column(),
+      ],
+    );
+  }
+}
