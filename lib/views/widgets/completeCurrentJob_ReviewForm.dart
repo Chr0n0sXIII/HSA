80f794d44c09e4bca6aa6129e8ccc36d712a5d3f
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-04-09 22:22:49 -0400

    padding

--- lib/views/widgets/completeCurrentJob_ReviewForm.dart
+++ lib/views/widgets/completeCurrentJob_ReviewForm.dart
@@ -47,64 +47,76 @@ class _Complete_Job_FormState extends State<Complete_Job_Form> {
         ),
         Column(
           children: [
-            Text(
-              'Client Review',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            Container(
-              width: 500,
-              decoration: BoxDecoration(boxShadow: [
-                BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: Offset(7.0, 8.0))
-              ]),
-              child: TextField(
-                maxLines: 5,
-                maxLength: 250,
-                decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    filled: true,
-                    hintText: 'Write your review...',
-                    errorText: review_Error,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5))),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Client Review',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
             ),
-            RatingBar.builder(
-                initialRating: 1,
-                minRating: 1,
-                direction: Axis.horizontal,
-                allowHalfRating: true,
-                itemCount: 5,
-                itemPadding: EdgeInsets.symmetric(horizontal: 4),
-                itemBuilder: (context, _) => Icon(
-                      Icons.star,
-                      color: Color.fromRGBO(195, 166, 96, 1),
-                    ),
-                onRatingUpdate: (rating) {
-                  print(rating);
-                }
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: 500,
+                decoration: BoxDecoration(boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(7.0, 8.0))
+                ]),
+                child: TextField(
+                  maxLines: 5,
+                  maxLength: 250,
+                  decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: 'Write your review...',
+                      errorText: review_Error,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5))),
+                ),
+              ),
             ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Color.fromRGBO(11, 206, 131, 1,),
-                shape: new RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(20)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RatingBar.builder(
+                  initialRating: 1,
+                  minRating: 1,
+                  direction: Axis.horizontal,
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  itemPadding: EdgeInsets.symmetric(horizontal: 4),
+                  itemBuilder: (context, _) => Icon(
+                        Icons.star,
+                        color: Color.fromRGBO(195, 166, 96, 1),
+                      ),
+                  onRatingUpdate: (rating) {
+                    print(rating);
+                  }
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromRGBO(11, 206, 131, 1,),
+                  shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(20)
+                  )
+                ),
+                onPressed: submit, 
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    'Submit',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
                 )
               ),
-              onPressed: submit, 
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Text(
-                  'Submit',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-              )
             )
           ],
         ),
