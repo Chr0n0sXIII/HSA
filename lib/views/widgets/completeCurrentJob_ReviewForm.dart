37ee6606af902ae92763fea7cb9b60e2c0972b8d
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-04-09 20:51:11 -0400

    additional packages

--- lib/views/widgets/completeCurrentJob_ReviewForm.dart
+++ lib/views/widgets/completeCurrentJob_ReviewForm.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'dart:io' show File;
+
 
 class Complete_Job_Form extends StatefulWidget {
   const Complete_Job_Form({ Key? key }) : super(key: key);
