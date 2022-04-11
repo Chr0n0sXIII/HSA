fb46b44cfc487ead3ddb557338baa2b4474676a6
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-04-11 04:20:52 -0400

    complete job button

--- lib/views/widgets/homeView_MapWidget.dart
+++ lib/views/widgets/homeView_MapWidget.dart
@@ -4,6 +4,8 @@ import 'package:google_maps/google_maps.dart' as gm;
 import 'dart:html';
 import 'dart:ui' as ui;
 
+import 'package:home_service_app/views/completeCurrentJobView.dart';
+
 class MapView extends StatefulWidget {
   const MapView({Key? key}) : super(key: key);
 
@@ -17,105 +19,125 @@ class _MapViewState extends State<MapView> {
   String jobDesc = 'Placeholder job description';
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 580,
-      width: 450,
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: const BorderRadius.all(Radius.circular(50)),
-          boxShadow: [
-            BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 5,
-                blurRadius: 7,
-                offset: Offset(7.0, 8.0))
-          ]),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            height: 80,
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(20, 25, 5, 0),
-              child: active_Job == false
-              ?Text(
-                'No Current Job Taken',
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 14,
-                ),
-              )
-              :Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 8),
-                    child: Text(
-                      job_Title,
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold
-                      ),
-                    ),
-                  ),
-                  Text(
-                    jobDesc,
-                    maxLines: 1,
-                    style: TextStyle(
-                      overflow: TextOverflow.ellipsis
-                    ),
-                  )
-                ],
-              )
-            ),
-          ),
-          Expanded(
-              flex: 1,
-              child: active_Job == false
-                  ? InkWell(
-                      onTap: () {},
-                      child: Container(
-                        decoration: const BoxDecoration(
-                            color: Color.fromRGBO(196, 196, 196, 1),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(45))),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Padding(
-                              padding: EdgeInsets.all(15),
-                              child: Text(
-                                'Looking For A Job',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
+    return Column(
+      children: [
+        Container(
+          height: 580,
+          width: 450,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: const BorderRadius.all(Radius.circular(50)),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(7.0, 8.0))
+              ]),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 80,
+                child: Padding(
+                    padding: EdgeInsets.fromLTRB(20, 25, 5, 0),
+                    child: active_Job == false
+                        ? Text(
+                            'No Current Job Taken',
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 14,
+                            ),
+                          )
+                        : Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 8),
+                                child: Text(
+                                  job_Title,
+                                  style: TextStyle(
+                                      fontSize: 20, fontWeight: FontWeight.bold),
                                 ),
                               ),
+                              Text(
+                                jobDesc,
+                                maxLines: 1,
+                                style: TextStyle(overflow: TextOverflow.ellipsis),
+                              )
+                            ],
+                          )),
+              ),
+              Expanded(
+                  flex: 1,
+                  child: active_Job == false
+                      ? InkWell(
+                          onTap: () {},
+                          child: Container(
+                            decoration: const BoxDecoration(
+                                color: Color.fromRGBO(196, 196, 196, 1),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(45))),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Padding(
+                                  padding: EdgeInsets.all(15),
+                                  child: Text(
+                                    'Looking For A Job',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  height: 50,
+                                  width: 50,
+                                  decoration: const BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(30))),
+                                  child: const Icon(
+                                    Icons.search_outlined,
+                                    color: Colors.grey,
+                                    size: 36,
+                                  ),
+                                )
+                              ],
                             ),
-                            Container(
-                              height: 50,
-                              width: 50,
-                              decoration: const BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(30))),
-                              child: const Icon(
-                                Icons.search_outlined,
-                                color: Colors.grey,
-                                size: 36,
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    )
-                  : Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(45))),
-                      child: GoogleMap(),
-                    ))
-        ],
-      ),
+                          ),
+                        )
+                      : Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(Radius.circular(45))),
+                          child: GoogleMap(),
+                        )),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: Color.fromRGBO(195, 166, 96, 1),
+                    shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(30))),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => Completed_Current_Job_View()));
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Text(
+                    'Complete Job',
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                )),
+        )
+      ],
     );
   }
 
