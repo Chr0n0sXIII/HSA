058258483f491d3530bbda7ffb60b09a42544031
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-04-12 13:39:18 -0400

    initial page

--- lib/views/editJobView.dart
+++ lib/views/editJobView.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:home_service_app/views/widgets/acceptWorkers_JobDesc.dart';
 import 'package:home_service_app/views/widgets/editJob_TileWidget.dart';
+import 'package:home_service_app/views/widgets/navBar.dart';
+import 'package:home_service_app/views/widgets/footer_Widget.dart';
+import 'package:home_service_app/views/widgets/pageTitle_Widget.dart';
 
-class editJobView extends StatelessWidget {
-  const editJobView({Key? key}) : super(key: key);
+import '../dataClasses/User.dart';
+
+class editJobView extends StatefulWidget {
+  final User user;
+  const editJobView({Key? key, required this.user}) : super(key: key);
 
   @override
+  State<editJobView> createState() => _editJobViewState();
+}
+
+class _editJobViewState extends State<editJobView> {
+  @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate:
-          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
-      itemBuilder: (_, index) =>
-          Container(height: 400, width: 400, child: JobDesc_edit()),
-      itemCount: 8,
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(229, 229, 229, 1),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TopBar(user: widget.user,),
+            PageTitle('View Your Current Jobs and Edit'),
+            Footer(),
+          ],
+        ),
+      ),
     );
   }
 }
