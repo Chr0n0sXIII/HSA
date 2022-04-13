9f8bb5a0f375e371338e543589885aaabf50d5a1
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-04-13 04:38:06 -0400

    initial page body

--- lib/views/jobListingView.dart
+++ lib/views/jobListingView.dart
@@ -1,90 +1,43 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:home_service_app/dataClasses/User.dart';
-import 'package:home_service_app/dataClasses/jobData.dart';
-import 'package:home_service_app/dataClasses/jobDataUtil.dart';
-import 'package:home_service_app/views/widgets/jobListing_Card.dart';
-import 'package:home_service_app/views/widgets/jobListing_MapWidget.dart';
-import 'package:home_service_app/views/widgets/jobListing_SearchWidget.dart';
-import 'package:home_service_app/views/widgets/navBar.dart';
+import 'package:home_service_app/views/widgets/footer_Widget.dart';
+import 'package:home_service_app/views/widgets/pageTitle_Widget.dart';
 
-class jobListingView extends StatelessWidget {
-  List<JobData> jobdata = [];
-  List<Image> images = [];
-  final User user;
+class jobListingView extends StatefulWidget {
+  const jobListingView({Key? key}) : super(key: key);
 
-  jobListingView({Key? key, required this.user}) : super(key: key);
+  @override
+  State<jobListingView> createState() => _jobListingViewState();
+}
+
+class _jobListingViewState extends State<jobListingView> {
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      body: Column(
-        children: [
-          TopBar(user: user,),
-          jobListing_SearchWidget(),
-          Expanded(
-            flex: 11,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Expanded(
-                      flex: 4,
-                      child: Column(
-                        children: [
-                          Text("Map PlaceHolder"),
-                        ],
-                      )),
-                  jobdata.isNotEmpty
-                
-                  ?Expanded(
-                    flex: 5,
-                    child: ListView.builder(
-                        itemCount: jobdata.length,
-                        itemBuilder: (BuildContext ctx, int index) {
-                          return JobListingCard(
-                            jobData: jobdata[index],
-                            image: images[index],
-                          );
-                        }),
-                  ):Text("Empty")
-                ],
-              ),
+      backgroundColor: Color.fromRGBO(229, 229, 229, 1),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            PageTitle('Job Listings'),
+            searchBar(),
+            Row(
+              children: [jobListingMapView(), jobList()],
             ),
-          ),
-        ],
+            Footer(),
+          ],
+        ),
       ),
     );
-    // TODO: implement build
-    throw UnimplementedError();
   }
 
-/*  loadJobs() async {
-    final value = await FirebaseFirestore.instance
-        .collection("jobs")
-        .get();
-    for (var doc in value.docs) {
-      String val = doc.get('Password');
-      //print(val.toString());
-      // print(passController.text);
-      print((passController.text.compareTo(val.toString())));
-      if (passController.text.compareTo(val.toString()) == 0) {
-        setState(() {
-          validated = true;
-        });
-        break;
-      } else {
-        setState(() {
-          validated = false;
-        });
-      }
-    }
-  }*/
+  searchBar() {
+    return Container();
+  }
 
-  initjobs() {
-    for (JobData j in JobDataUtil.TestData_JobsData()) {
-      FirebaseFirestore.instance.collection('openjobs').add(j.toMap());
-    }
+  jobListingMapView() {
+    return Container();
+  }
+
+  jobList() {
+    return Container();
   }
 }
