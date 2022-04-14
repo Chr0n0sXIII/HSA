a93ff600a937e13107c25278e4ce6dda9a71b957
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-04-13 23:39:18 -0400

    updated User Class

--- lib/dataClasses/User.dart
+++ lib/dataClasses/User.dart
@@ -1,35 +1,51 @@
-
 import 'package:flutter/material.dart';
 
 class User {
-  String uName = "Jiles Ramjattan";
-  String email = "jilesramjattan@gmail.com";
-  String about = "Na";
-  String contacts = "Na";
-  String skills = "";
-  List<String> activeRequests = [""];
-  List<String> completedRequests = [""];
-  List<String> completedJobs = [""];
-  String activeJob = "";
-  double clientRating = 0;
-  double workerRating = 0;
-  Image pfpImage = Image.asset("assets/profile_picture_place_holder.png");
-  String pfp = "";
+  late String user_ID;
+  late String uName;
+  late String userPassword;
+  late String email;
+  late String about;
+  late String contacts;
+  late String skills;
+  late List<String> activeJobs;
+  late List<String> completedRequests;
+  late List<String> completedJobs;
+  late String currentJobTaken;
+  late double userRating;
+  late String pfp;
 
-  User(
-      {required this.uName,
-      required this.email,
-      required this.about,
-      required this.contacts,
-      required this.skills,
-      required this.activeRequests,
-      required this.completedRequests,
-      required this.activeJob,
-      required this.completedJobs,
-      required this.clientRating,
-      required this.workerRating,
-      required this.pfp,
-      required this.pfpImage});
+  User({
+    required this.user_ID,
+    required this.uName,
+    required this.userPassword,
+    required this.email,
+    required this.about,
+    required this.contacts,
+    required this.skills,
+    required this.activeJobs,
+    required this.completedRequests,
+    required this.currentJobTaken,
+    required this.completedJobs,
+    required this.userRating,
+    required this.pfp,
+  });
+
+  User.register(String userID, String name, String Uemail, String password) {
+    user_ID = userID;
+    uName = name;
+    email = Uemail;
+    userPassword = password;
+    about = "Na";
+    contacts = "Na";
+    skills = "Na";
+    activeJobs = [""];
+    completedRequests = [""];
+    completedJobs = [""];
+    currentJobTaken = "";
+    userRating = 0;
+    pfp = "";
+  }
 
   setUname(String uName) {
     this.uName = uName;
@@ -51,42 +67,43 @@ class User {
     this.skills = skills;
   }
 
-  setactiveRequest(List<String> Requests) {
-    this.activeRequests = Requests;
+  setactiveRequest(List<String> activeJobs) {
+    this.activeJobs = activeJobs;
   }
 
-  setactiveJob(String job) {
-    this.activeJob = job;
+  setactiveJob(String currentJobTaken) {
+    this.currentJobTaken = currentJobTaken;
   }
 
   setclientRating(var clientRating) {
-    this.clientRating = clientRating;
-  }
-
-  setworkerRating(var workerRating) {
-    this.workerRating = workerRating;
+    this.userRating = clientRating;
   }
 
   setpfp(String pfp) {
     this.pfp = pfp;
   }
 
-   toMap() {
+  Map<String, dynamic> toJson() {
     return {
-      uName: uName,
-      email: email,
-      about: about,
-      contacts: contacts,
-      skills: skills,
-      activeRequests: activeRequests.toList(growable: false),
-      completedRequests: completedRequests,
-      activeJob: activeJob,
-      completedJobs: completedJobs,
-      clientRating: clientRating,
-      workerRating: workerRating,
-      pfp: pfp
+      'ID' : user_ID,
+      'Name' : uName,
+      'Email' : email,
+      'Password' : userPassword,
+      'About' : about,
+      'Contacts' : contacts,
+      'Skills' : skills,
+      'Active_Jobs' : activeJobs,
+      'Completed_Request' : completedRequests,
+      'Curresnt_Job_Taken' : currentJobTaken,
+      'User_Rating' : userRating,
+      'Profile_Picture' : pfp
     };
   }
 
-
+  @override
+  String toString() {
+    // TODO: implement toString
+    print('\nID : ' + this.user_ID + '\nName : ' + this.uName + '\nEmail : ' + this.email + '\nContact : ' + this.contacts + '\nAbout : ' + this.about + '\nSkils : ' + this.skills + '\nRating : ' + this.userRating.toString());
+    return super.toString();
+  }
 }
