1461402505c3d9772a698c4e6a2bf2bed5b599c0
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-04-14 22:50:47 -0400

    pass job data

--- lib/views/jobDetailsView.dart
+++ lib/views/jobDetailsView.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:home_service_app/dataClasses/jobData.dart';
+import 'package:home_service_app/views/widgets/addJob_FormWidget.dart';
 import 'package:home_service_app/views/widgets/jobDetails_ImageWidget.dart';
 import 'package:home_service_app/views/widgets/jobDetails_InfoWidget.dart';
 import 'package:home_service_app/views/widgets/navBar.dart';
@@ -9,25 +11,24 @@ import '../dataClasses/User.dart';
 
 class jobDetailsView extends StatelessWidget {
   final User user;
-  List<String> imageURL_List = <String>[
-    'https://picsum.photos/id/237/200/300',
-    'https://picsum.photos/id/237/200/300'
-  ];
-  jobDetailsView({Key? key,required this.user}) : super(key: key);
+  final JobData job;
+  jobDetailsView({Key? key, required this.user, required this.job}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         child: Column(children: [
-          TopBar(user: user,),
+          TopBar(
+            user: user,
+          ),
           PageTitle(" Job Details"),
           Padding(
-            padding: const EdgeInsets.only(left: 250,right: 250),
+            padding: const EdgeInsets.only(left: 250, right: 250),
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                jobDetails_ImageWidget(imagelist: imageURL_List),
-                jobDetails_InfoWidget(user: user),
+                jobDetails_ImageWidget(imagelist: job.ActiveJobImages),
+                jobDetails_InfoWidget(job: job,user: user,),
               ],
             ),
           )
