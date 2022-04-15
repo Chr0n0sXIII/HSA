dc1b19973762957b7f3f18449e79f2edac6b5ae4
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-04-14 23:16:29 -0400

    get jobs

--- lib/views/widgets/editJob_TileWidget.dart
+++ lib/views/widgets/editJob_TileWidget.dart
@@ -1,52 +1,76 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:home_service_app/dataClasses/jobData.dart';
+
+import '../../dataClasses/User.dart';
 
 class editJobTileListing extends StatefulWidget {
-  const editJobTileListing({ Key? key }) : super(key: key);
+  final User user;
+  const editJobTileListing({Key? key, required this.user}) : super(key: key);
 
   @override
   State<editJobTileListing> createState() => _editJobTileListingState();
 }
 
 class _editJobTileListingState extends State<editJobTileListing> {
+  List<JobData> allJobs = [];
   List<String> ImageURL_list = <String>[];
   String j_title = 'Placeholder Title';
   String j_description = 'Placeholder Description';
   String j_location = 'Placeholder Location';
   String j_price = 'Placeholer Price';
   bool recievedImages = false;
-  int total_Jobs = 5;
+  int total_Jobs = 0;
   @override
   Widget build(BuildContext context) {
+    getJobs();
     return Padding(
       padding: const EdgeInsets.fromLTRB(250, 8, 250, 8),
       child: Column(
         children: [
           total_Jobs >= 1
-          ?GridView.builder(
-              shrinkWrap: true,
-              itemCount: total_Jobs,
-              gridDelegate:
-                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-              itemBuilder: (context, index) => edit_Tile(
-                  j_title,
-                  j_description,
-                  j_location,
-                  j_price,
-                  recievedImages,
-                  ImageURL_list))
-          :Text(
-            'You Have No Active Jobs',
-            style: TextStyle(
-              fontSize: 40
-            ),
-          )
+              ? GridView.builder(
+                  shrinkWrap: true,
+                  itemCount: total_Jobs,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3),
+                  itemBuilder: (context, index) => edit_Tile(
+                      j_title,
+                      j_description,
+                      j_location,
+                      j_price,
+                      recievedImages,
+                      ImageURL_list))
+              : Text(
+                  'You Have No Active Jobs',
+                  style: TextStyle(fontSize: 40),
+                )
         ],
       ),
     );
   }
 
-  edit_Tile(String j_title, String j_description, String j_location, String j_price, bool recievedImages, List<String> workerImageURL_list) {
+  getJobs() async {
+    List<String> ids = widget.user.activeJobs;
+    var docJob;
+    var snapshot;
+    JobData job;
+    List<JobData> jobs = [];
+    for (int i = 1; i < ids.length; i++) {
+      docJob = FirebaseFirestore.instance.collection('jobs').doc(ids[i]);
+      snapshot = await docJob.get();
+      job = JobData.fromJson(snapshot.data());
+      jobs.add(job);
+    }
+    setState(() {
+      total_Jobs = allJobs.length;
+      allJobs = jobs;
+    });
+  }
+
+  edit_Tile(String j_title, String j_description, String j_location,
+      String j_price, bool recievedImages, List<String> workerImageURL_list) {
     return Container(
       margin: EdgeInsets.all(25),
       width: 400,
@@ -111,7 +135,9 @@ class _editJobTileListingState extends State<editJobTileListing> {
                 Text(
                   j_price,
                   style: TextStyle(
-                      fontSize: 18, fontWeight: FontWeight.bold, color: Colors.red),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red),
                 ),
               ],
             ),
@@ -122,45 +148,35 @@ class _editJobTileListingState extends State<editJobTileListing> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.all(15),
-                    primary: Color.fromRGBO(54, 153, 244, 1),
-                    shape: new RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(30)
-                    )
-                  ),
-                  onPressed: editThisJob,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(15, 8, 15, 8),
-                    child: Text(
-                      'Edit',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold
+                    style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.all(15),
+                        primary: Color.fromRGBO(54, 153, 244, 1),
+                        shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(30))),
+                    onPressed: editThisJob,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(15, 8, 15, 8),
+                      child: Text(
+                        'Edit',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
                       ),
-                    ),
-                  )
-                ),
+                    )),
                 ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.all(15),
-                    primary: Color.fromRGBO(244, 67, 54, 1),
-                    shape: new RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(30)
-                    )
-                  ),
-                  onPressed: removeThisJob,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(10, 8, 10, 8),
-                    child: Text(
-                      'Remove',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold
+                    style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.all(15),
+                        primary: Color.fromRGBO(244, 67, 54, 1),
+                        shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(30))),
+                    onPressed: removeThisJob,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 8, 10, 8),
+                      child: Text(
+                        'Remove',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
                       ),
-                    ),
-                  )
-                )
+                    ))
               ],
             ),
           )
