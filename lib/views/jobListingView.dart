d86f5e9cb48f43e4858bb38c8730e6ca16626e99
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-04-14 22:50:30 -0400

    onClick for job tile

--- lib/views/jobListingView.dart
+++ lib/views/jobListingView.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:home_service_app/dataClasses/jobData.dart';
+import 'package:home_service_app/views/jobDetailsView.dart';
 import 'package:home_service_app/views/widgets/footer_Widget.dart';
 import 'package:home_service_app/views/widgets/navBar.dart';
 import 'package:home_service_app/views/widgets/pageTitle_Widget.dart';
@@ -217,7 +218,6 @@ class _jobListingViewState extends State<jobListingView> {
         ..style.borderRadius = '13px';
 
       final map = gm.GMap(elem, mapOptions);
-
       gm.Marker(gm.MarkerOptions()
         ..position = myLatlng
         ..map = map
@@ -232,65 +232,79 @@ class _jobListingViewState extends State<jobListingView> {
   Widget jobTile(JobData job) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 10),
-      child: Container(
-        height: 150,
-        width: 350,
-        decoration: BoxDecoration(
-            color: Colors.white, borderRadius: BorderRadius.circular(10)),
-        child: Row(
-          children: [
-            SizedBox(
-              height: 150,
-              width: 150,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.network(
-                  job.ActiveJobImages[0],
-                  fit: BoxFit.fill,
+      child: InkWell(
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+        hoverColor: Color.fromRGBO(195, 166, 96, 0.25),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => jobDetailsView(
+                user: widget.user, job: job,
+              )
+            )
+          );
+        },
+        child: Container(
+          height: 150,
+          width: 350,
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(10)),
+          child: Row(
+            children: [
+              SizedBox(
+                height: 150,
+                width: 150,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(
+                    job.ActiveJobImages[0],
+                    fit: BoxFit.fill,
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    job.jobName,
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    job.jobDescription,
-                    maxLines: 1,
-                    style: TextStyle(
-                      fontSize: 16,
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      job.jobName,
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                     ),
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        job.jobLocation,
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
+                    Text(
+                      job.jobDescription,
+                      maxLines: 1,
+                      style: TextStyle(
+                        fontSize: 16,
                       ),
-                      Icon(
-                        Icons.pin_drop,
-                        color: Colors.red,
-                      )
-                    ],
-                  ),
-                  Text(
-                    '\$ ' + job.jobPrice,
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.red),
-                  ),
-                ],
-              ),
-            )
-          ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          job.jobLocation,
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        Icon(
+                          Icons.pin_drop,
+                          color: Colors.red,
+                        )
+                      ],
+                    ),
+                    Text(
+                      '\$ ' + job.jobPrice,
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
