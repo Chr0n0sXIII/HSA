43903a426c671076f7d6805a3116d4aa2b486e0c
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-04-17 16:07:34 -0400

    box shadow

--- lib/views/widgets/accepWorkersList_widget.dart
+++ lib/views/widgets/accepWorkersList_widget.dart
@@ -66,7 +66,15 @@ class _Active_Jobs_ListState extends State<Active_Jobs_List> {
         margin: EdgeInsets.all(25),
         width: 400,
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15), color: Colors.white),
+            borderRadius: BorderRadius.circular(15), color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(7.0, 8.0))
+            ]
+            ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
