87d912e0734f4aca34cb1273ace133760d44cdf2
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-04-17 12:44:44 -0400

    display data

--- lib/views/widgets/acceptWorkers_JobDesc.dart
+++ lib/views/widgets/acceptWorkers_JobDesc.dart
@@ -4,7 +4,8 @@ import 'package:home_service_app/dataClasses/jobData.dart';
 
 class accpectWorkerJobDescription extends StatefulWidget {
   final JobData job;
-  const accpectWorkerJobDescription({Key? key, required this.job}) : super(key: key);
+  const accpectWorkerJobDescription({Key? key, required this.job})
+      : super(key: key);
 
   @override
   State<accpectWorkerJobDescription> createState() =>
@@ -13,13 +14,17 @@ class accpectWorkerJobDescription extends StatefulWidget {
 
 class _accpectWorkerJobDescriptionState
     extends State<accpectWorkerJobDescription> {
-  List<String> ImageURL_list = <String>[];
-  String j_title = 'Placeholder Title';
-  String j_description = 'Placeholder Description';
-  String j_location = 'Placeholder Location';
-  String j_price = 'Placeholer Price';
   bool imageLoaded = false;
-  int total_Jobs = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    if (widget.job.ActiveJobImages.length > 0) {
+      imageLoaded = true;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -38,9 +43,9 @@ class _accpectWorkerJobDescriptionState
                   child: Center(child: CircularProgressIndicator()),
                 )
               : CarouselSlider.builder(
-                  itemCount: ImageURL_list.length,
+                  itemCount: widget.job.ActiveJobImages.length,
                   itemBuilder: (context, index, realIndex) {
-                    final ImageURL = ImageURL_list[index];
+                    final ImageURL = widget.job.ActiveJobImages[index];
                     return buildImage(ImageURL, index);
                   },
                   options: CarouselOptions(
@@ -52,14 +57,14 @@ class _accpectWorkerJobDescriptionState
           Padding(
             padding: const EdgeInsets.fromLTRB(15, 8, 8, 8),
             child: Text(
-              j_title,
+              widget.job.jobName,
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ),
           Padding(
             padding: const EdgeInsets.fromLTRB(15, 8, 8, 8),
             child: Text(
-              j_description,
+              widget.job.jobDescription,
             ),
           ),
           Padding(
@@ -67,7 +72,7 @@ class _accpectWorkerJobDescriptionState
             child: Row(
               children: [
                 Text(
-                  j_location,
+                  widget.job.jobLocation,
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
                 Icon(
@@ -80,7 +85,7 @@ class _accpectWorkerJobDescriptionState
           Padding(
             padding: const EdgeInsets.fromLTRB(15, 8, 8, 8),
             child: Text(
-              j_price,
+              '\$ ' + widget.job.jobPrice,
               style: TextStyle(
                   fontSize: 18, fontWeight: FontWeight.bold, color: Colors.red),
             ),
@@ -92,6 +97,7 @@ class _accpectWorkerJobDescriptionState
 
   Widget buildImage(String imageURL, int index) {
     return Container(
+      width: 400,
       margin: EdgeInsets.symmetric(horizontal: 5),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(15),
