8d2fe938c43f14b4b952b539b4747b955b7b552e
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2022-04-19 00:53:47 -0400

    update userpage to db

--- lib/views/widgets/userProfile_FormWidget.dart
+++ lib/views/widgets/userProfile_FormWidget.dart
@@ -1,10 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:uuid/uuid.dart';
 import 'dart:io';
 import 'dart:io' show File;
 
+import '../../dataClasses/User.dart';
+import '../homeView.dart';
+
 class ProfileForm extends StatefulWidget {
-  const ProfileForm({Key? key}) : super(key: key);
+  final User user;
+  const ProfileForm({Key? key, required this.user}) : super(key: key);
 
   @override
   State<ProfileForm> createState() => _ProfileFormState();
@@ -12,13 +19,14 @@ class ProfileForm extends StatefulWidget {
 
 class _ProfileFormState extends State<ProfileForm> {
   String? username_Error;
-  String currentUser_Username = "demo_Username";
   String? AboutMe_Error;
-  String currentUser_AboutMe = "demo_AboutMe";
   String? TradeSkills_Error;
-  String currentUser_TradeSkils = "demo_TradeSkills";
   String? contactInfo_Error;
-  String currentUser_Contact = "demo_Contact";
+
+  final nameController = TextEditingController();
+  final aboutmeController = TextEditingController();
+  final skillsController = TextEditingController();
+  final contactController = TextEditingController();
 
   final ImagePicker _picker = ImagePicker();
   File? file;
@@ -26,6 +34,8 @@ class _ProfileFormState extends State<ProfileForm> {
   List<XFile> ImageList = <XFile>[];
   List<String> imageURL_list = <String>[];
   bool imageLoaded = false;
+
+  late String value;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -52,8 +62,11 @@ class _ProfileFormState extends State<ProfileForm> {
                                 offset: Offset(7.0, 8.0))
                           ]),
                       child: imageLoaded == false
-                          ? Image.asset("profile_picture_place_holder.png")
-                          : Image.network(imageURL_list[0]))),
+                          ? Image.network(widget.user.pfp)
+                          : Image.network(
+                              imageURL_list[0],
+                              fit: BoxFit.cover,
+                            ))),
               Padding(
                 padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
                 child: ElevatedButton(
@@ -100,13 +113,14 @@ class _ProfileFormState extends State<ProfileForm> {
                       offset: const Offset(1.0, 1.0))
                 ]),
                 child: TextField(
+                  controller: nameController,
                   maxLines: 1,
                   maxLength: 20,
                   decoration: InputDecoration(
                       fillColor: Colors.white,
                       filled: true,
                       errorText: username_Error,
-                      hintText: currentUser_Username,
+                      hintText: widget.user.uName,
                       border: const OutlineInputBorder(
                           borderRadius: BorderRadius.all(Radius.circular(5)))),
                 ),
@@ -141,12 +155,13 @@ class _ProfileFormState extends State<ProfileForm> {
                       offset: const Offset(1.0, 1.0))
                 ]),
                 child: TextField(
+                  controller: aboutmeController,
                   maxLines: 4,
                   maxLength: 250,
                   decoration: InputDecoration(
                       fillColor: Colors.white,
                       filled: true,
-                      hintText: currentUser_AboutMe,
+                      hintText: widget.user.about,
                       errorText: AboutMe_Error,
                       border: const OutlineInputBorder(
                           borderRadius: BorderRadius.all(Radius.circular(5)))),
@@ -182,13 +197,14 @@ class _ProfileFormState extends State<ProfileForm> {
                       offset: const Offset(1.0, 1.0))
                 ]),
                 child: TextField(
+                  controller: skillsController,
                   maxLines: 2,
                   maxLength: 80,
                   decoration: InputDecoration(
                       fillColor: Colors.white,
                       filled: true,
                       errorText: TradeSkills_Error,
-                      hintText: currentUser_TradeSkils,
+                      hintText: widget.user.skills,
                       border: const OutlineInputBorder(
                           borderRadius: BorderRadius.all(Radius.circular(5)))),
                 ),
@@ -225,13 +241,14 @@ class _ProfileFormState extends State<ProfileForm> {
                       offset: Offset(1.0, 1.0))
                 ]),
                 child: TextField(
+                  controller: contactController,
                   maxLines: 1,
                   maxLength: 30,
                   decoration: InputDecoration(
                       fillColor: Colors.white,
                       filled: true,
                       errorText: contactInfo_Error,
-                      hintText: currentUser_Contact,
+                      hintText: widget.user.contacts,
                       border: const OutlineInputBorder(
                           borderRadius: BorderRadius.all(Radius.circular(5)))),
                 ),
@@ -245,7 +262,9 @@ class _ProfileFormState extends State<ProfileForm> {
           child: ElevatedButton(
               style: ElevatedButton.styleFrom(
                   primary: Color.fromRGBO(11, 206, 131, 1)),
-              onPressed: () {},
+              onPressed: () {
+                update();
+              },
               child: const Padding(
                 padding: EdgeInsets.all(8.0),
                 child: Text(
@@ -276,5 +295,61 @@ class _ProfileFormState extends State<ProfileForm> {
     for (var bytes in image!) {
       imageURL_list.add(File(bytes.path).path.toString());
     }
+  }
+
+  Future<void> update() async {
+    final docUser =
+        FirebaseFirestore.instance.collection('users').doc(widget.user.user_ID);
+    if (nameController.text.isNotEmpty) {
+      docUser.update({'Name': nameController.text});
+      widget.user.setUname(nameController.text);
+    }
+    if (aboutmeController.text.isNotEmpty) {
+      docUser.update({'About': aboutmeController.text});
+      widget.user.setabout(aboutmeController.text);
+    }
+    if (skillsController.text.isNotEmpty) {
+      docUser.update({'Skills': skillsController.text});
+      widget.user.setskills(skillsController.text);
+    }
+    if (nameController.text.isNotEmpty) {
+      docUser.update({'Contacts': contactController.text});
+      widget.user.setcontacts(contactController.text);
+    }
+    if (imageLoaded == true) {
+      await upload(widget.user.user_ID);
+      widget.user.setpfp(value);
+      docUser.update({'Profile_Picture': value});
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => HomeView(
+                  user: widget.user,
+                )));
+    }
+  }
+
+  upload(String jobID) async {
+    String productId = await uploadImageAndSaveItemInfo(jobID);
+  }
+
+  Future<String> uploadImageAndSaveItemInfo(String userID) async {
+    PickedFile? pickedFile;
+    String? productId = userID;
+    file = File(ImageList[0].path);
+    pickedFile = PickedFile(file!.path);
+    await uploadImageToStorage(pickedFile, productId);
+    return productId;
+  }
+
+  uploadImageToStorage(PickedFile? pickedFile, String productId) async {
+    String? pId = const Uuid().v4();
+    Reference reference =
+        FirebaseStorage.instance.ref().child('PFPs/$productId/$pId');
+    await reference.putData(
+      await pickedFile!.readAsBytes(),
+      SettableMetadata(contentType: 'image/jpeg'),
+    );
+    value = await reference.getDownloadURL();
   }
 }
